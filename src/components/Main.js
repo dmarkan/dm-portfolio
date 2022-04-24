2b646eb51e2b9d27273fd648d8c0deaca7ada7fe
Darko Markan <75615049+dmarkan@users.noreply.github.com>
2022-04-24 21:05:31 +0200

    navbar done

---
+++ src/components/Main.js
@@ -0,0 +1,13 @@
+import React from "react";
+
+const Main = () => {
+    return (
+        <div class="container-fluid mt-1">
+        <img class="img-fluid rounded mx-auto d-block" src="images/header-img.jpg" alt="header-img" />
+        <h4 class="text-center mt-1">Hello! My name is Darko Markan.</h4>
+        <p class="fst-italic text-center">I'm a <span class="pro-span">pro</span>grammer.</p>
+        </div>
+    )
+}
+
+export default Main;
---
+++ src/components/Navbar.js
@@ -0,0 +1,29 @@
+import React from "react";
+
+const Navbar = () => {
+    return (
+        <nav id="about" class="navbar navbar-expand-lg navbar navbar-dark bg-dark">
+        <div class="container-fluid">
+        <a class="navbar-brand" href="#">Darko Markan</a>
+          <button class="navbar-toggler" type="button" data-bs-toggle="collapse" data-bs-target="#navbarNav" aria-controls="navbarNav" aria-expanded="false" aria-label="Toggle navigation">
+            <span class="navbar-toggler-icon"></span>
+          </button>
+          <div class="collapse navbar-collapse nav" id="navbarNav">
+            <ul class="navbar-nav ms-auto">
+              <li class="nav-item">
+                <a class="nav-link active" aria-current="page" href="/">About</a>
+              </li>
+              <li class="nav-item">
+                <a class="nav-link" href="#">Works</a>
+              </li>
+              <li class="nav-item">
+                <a class="nav-link" href="#">Contact</a>
+              </li>
+            </ul>
+          </div>
+        </div>
+      </nav>
+    )
+}
+
+export default Navbar;

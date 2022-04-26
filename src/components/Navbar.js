69add7479b3919151835eaf3c275e12a3c2de439
Darko Markan <75615049+dmarkan@users.noreply.github.com>
2022-04-26 19:31:57 +0200

    home bug fixed

--- src/components/Navbar.js
+++ src/components/Navbar.js
@@ -4,7 +4,7 @@ const Navbar = () => {
     return (
         <nav id="home" class="navbar navbar-expand-lg navbar navbar-dark bg-dark">
         <div class="container-fluid">
-        <a class="navbar-brand" href="#">Darko Markan</a>
+        <a class="navbar-brand" href="#home">Darko Markan</a>
           <button class="navbar-toggler" type="button" data-bs-toggle="collapse" data-bs-target="#navbarNav" aria-controls="navbarNav" aria-expanded="false" aria-label="Toggle navigation">
             <span class="navbar-toggler-icon"></span>
           </button>
